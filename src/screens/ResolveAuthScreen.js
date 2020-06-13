617c7027faa17189a73a1aba7970e4395e1601fd
Brandon Zhu <bzhu@utraque.com>
2020-06-13 13:30:50 -0400

    217. Empty Screens While Resolving Auth

---
+++ src/screens/ResolveAuthScreen.js
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react';
+import{Context as AuthContext} from '../context/AuthContext';
+
+const ResolveAuthScreen = ()=> {
+  const { tryLocalSignin} = useContext(AuthContext);
+
+  useEffect(() => {
+    tryLocalSignin();
+  }, []);
+
+  return null;
+}
+
+export default ResolveAuthScreen;
