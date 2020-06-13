1eea8d163469dd0fdb67dac11f508ae8635767d2
Brandon Zhu <bzhu@utraque.com>
2020-06-13 15:36:54 -0400

    220. Working on Track Create

---
+++ src/components/Map.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from 'react';
+import {Test, StyleSheet} from 'react-native';
+
+const Map=()=> {
+  return <Text>Map</Text>
+};
+
+const styles = StyleSheet.create({});
+
+export default Map;
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import { SafeAreaView } from 'react-navigation';
+import { StyleSheet } from 'react-native';
+import { Text } from 'react-native-elements';
+import Map from '../components/Map';
 
-const TrackCreateScreen = ()=> {
-  return <Text style={{fontSize: 48}}>TrackCreateScreen</Text>;
+const TrackCreateScreen = () => {
+  return <SafeAreaView forceInset={{ top: 'always' }}>
+    <Text h2>Create a Track</Text>
+    <Map />
+  </SafeAreaView>
 };
 
 const styles = StyleSheet.create({});
