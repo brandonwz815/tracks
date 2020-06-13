cede1eda3b56643339e7687df0e810b49525553b
Brandon Zhu <bzhu@utraque.com>
2020-06-13 15:52:42 -0400

    222.Showing a Map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,22 @@
-import React, { PureComponent } from 'react';
-import {Test, StyleSheet} from 'react-native';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
 
-const Map=()=> {
-  return <Text>Map</Text>
+const Map = () => {
+  return <MapView 
+  style={styles.map} 
+  initialRegion={{
+    latitude: 37.33233,
+    longitude: -122.03121,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01
+  }}/>
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    height: 300
+  }
+});
 
 export default Map;
