3a1a331212b6f61a5467814aa32904e53442e888
Brandon Zhu <bzhu@utraque.com>
2020-06-14 12:10:04 -0400

    233. Live Location Updates

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,12 +1,14 @@
 import '../_mockLocation';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { SafeAreaView } from 'react-navigation';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
 import Map from '../components/Map';
+import { Context as LocationContext } from '../context/LocationContext';
 
 const TrackCreateScreen = () => {
+  const { addLocation } = useContext(LocationContext);
   const [err, setErr] = useState(null);
 
   const startWatching = async () => {
@@ -16,8 +18,8 @@ const TrackCreateScreen = () => {
         accuracy: Accuracy.BestForNavigation,
         timeInterval: 1000,
         distanceInterval: 10
-      }, location=> {
-        console.log(location)
+      }, location => {
+        addLocation(location);
       })
     } catch (e) {
       setErr(e);
@@ -27,11 +29,11 @@ const TrackCreateScreen = () => {
   useEffect(() => {
     startWatching();
   }, []);
-console.log(err)
+
   return <SafeAreaView forceInset={{ top: 'always' }}>
     <Text h2>Create a Track</Text>
     <Map />
-    {/* {err ? <Text>Please enable location services</Text> : null}; */}
+    {err ? <Text>Please enable location services</Text> : null}
   </SafeAreaView>
 };
 
