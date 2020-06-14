e556f95994fd8383cf1c7437347d07121d5c2e58
Brandon Zhu <bzhu@utraque.com>
2020-06-14 12:43:20 -0400

    235. Extracting Logic to a Hook

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -3,32 +3,13 @@ import React, { useState, useEffect, useContext } from 'react';
 import { SafeAreaView } from 'react-navigation';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
-import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
 import Map from '../components/Map';
 import { Context as LocationContext } from '../context/LocationContext';
+import useLocation from '../hooks/useLocation';
 
 const TrackCreateScreen = () => {
   const { addLocation } = useContext(LocationContext);
-  const [err, setErr] = useState(null);
-
-  const startWatching = async () => {
-    try {
-      await requestPermissionsAsync();
-      await watchPositionAsync({
-        accuracy: Accuracy.BestForNavigation,
-        timeInterval: 1000,
-        distanceInterval: 10
-      }, location => {
-        addLocation(location);
-      })
-    } catch (e) {
-      setErr(e);
-    }
-  };
-
-  useEffect(() => {
-    startWatching();
-  }, []);
+  const [err] = useLocation(addLocation);
 
   return <SafeAreaView forceInset={{ top: 'always' }}>
     <Text h2>Create a Track</Text>
