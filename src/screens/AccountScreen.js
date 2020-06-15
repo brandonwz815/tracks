4f1ae5c78743f292d91c25cc2f71f82901edea6c
Brandon Zhu <bzhu@utraque.com>
2020-06-15 00:22:54 -0400

    Fixing Odds and Ends

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -4,16 +4,22 @@ import { Button } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
 import Spacer from '../components/Spacer';
 import { Context as AuthContext } from '../context/AuthContext';
+import { FontAwesome } from '@expo/vector-icons';
 
 const AccountScreen = () => {
   const { signout } = useContext(AuthContext);
-  return <SafeAreaView forceInset={{top: 'always'}}>
+  return <SafeAreaView forceInset={{ top: 'always' }}>
     <Text style={{ fontSize: 48 }}>AccountScreen</Text>
     <Spacer>
       <Button title="Sign Out" onPress={signout} />
     </Spacer>
   </SafeAreaView>
 };
+
+AccountScreen.navigationOptions = {
+  title: 'Account',
+  tabBarIcon: <FontAwesome name="gear" size={20} />
+}
 
 const styles = StyleSheet.create({});
 
