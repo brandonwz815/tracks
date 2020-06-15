fd7920f4a5e03b78353e2a69a47eb90782c290d2
Brandon Zhu <bzhu@utraque.com>
2020-06-14 22:25:36 -0400

    250. Coordination Between Contexts

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 import { Context as LocationContext } from '../context/LocationContext';
+import useSaveTrack from '../hooks/useSaveTrack';
 
 const TrackForm = () => {
   const {
@@ -11,20 +12,22 @@ const TrackForm = () => {
     changeName
   } = useContext(LocationContext);
 
+  const [saveTrack] = useSaveTrack();
+
   return <>
     <Spacer>
       <Input placeholder="Enter name" onChangeText={changeName} />
     </Spacer>
-    <Spacer>    
-    {recording ? (
-      <Button title="Stop" onPress={stopRecording} />
-    ) : (
-        <Button title="Start Recording" onPress={startRecording} />
-      )
-    }
+    <Spacer>
+      {recording ? (
+        <Button title="Stop" onPress={stopRecording} />
+      ) : (
+          <Button title="Start Recording" onPress={startRecording} />
+        )
+      }
     </Spacer>
     <Spacer>
-    {!recording && locations.length ? <Button title="Save Recording" /> : null}
+      {!recording && locations.length ? <Button title="Save Recording" onPress={saveTrack} /> : null}
     </Spacer>
   </>
 };
---
+++ src/hooks/useSaveTrack.js
@@ -0,0 +1,14 @@
+import { useState, useEffect, useContext } from 'react';
+import { Context as LocationContext } from '../context/LocationContext';
+import { Context as TrackContext } from '../context/TrackContext';
+
+export default () => {
+  const { createTrack } = useContext(TrackContext);
+  const { state: { locations, name } } = useContext(LocationContext);
+
+  const saveTrack = () => {
+    createTrack(name, locations);
+  };
+
+  return [saveTrack];
+};
