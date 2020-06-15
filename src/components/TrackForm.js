ae8bca6e0b8b0ceafa4f35da4af2c4f3b12b2689
Brandon Zhu <bzhu@utraque.com>
2020-06-14 22:07:22 -0400

    249. What Manages Tracks

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -11,18 +11,21 @@ const TrackForm = () => {
     changeName
   } = useContext(LocationContext);
 
-  console.log(locations.length)
-
   return <>
     <Spacer>
       <Input placeholder="Enter name" onChangeText={changeName} />
     </Spacer>
+    <Spacer>    
     {recording ? (
       <Button title="Stop" onPress={stopRecording} />
     ) : (
         <Button title="Start Recording" onPress={startRecording} />
       )
     }
+    </Spacer>
+    <Spacer>
+    {!recording && locations.length ? <Button title="Save Recording" /> : null}
+    </Spacer>
   </>
 };
 
